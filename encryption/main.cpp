067ec1b735d36a51e67910856526c3a893276305
Floyzen <107361187+FloyzenCode@users.noreply.github.com>
2023-02-07 20:29:22 +0400

    Add files via upload

--- encryption/main.cpp
+++ encryption/main.cpp
@@ -4,6 +4,7 @@
 #define _STD_USE_ using namespace std;
 constexpr auto FOUR = 4;
 constexpr auto sizearr = 1024;
+
 _STD_USE_
 
 // Структура функции SetWindowsPos с официального сайта Microsoft
@@ -30,6 +31,7 @@ public:
 	setlocale(NULL, "");
 	system("chcp 1251 > null");
 }
+
 private:
 	HANDLE console = GetStdHandle(STD_OUTPUT_HANDLE);
 	WORD color = FOREGROUND_BLUE | FOREGROUND_RED | FOREGROUND_INTENSITY;
@@ -81,6 +83,12 @@ public:
 	
 	// Шифрование и дешифрование
 	char de_and_encryption() {
+		INT de_encryptionNumber = rand() % 100;
+		//	color	//	
+		WORD forIndex = FOREGROUND_RED | BACKGROUND_INTENSITY;
+		SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), forIndex);
+		cout << "Encryption index: " << de_encryptionNumber << endl;
+		sys._config();
 		while (true) {
 			if (GetAsyncKeyState(VK_F1))
 				break;
@@ -88,10 +96,10 @@ public:
 			cout << "Введите текст: ";
 			cin.getline(str, sizearr);
 			for (int i = NULL; str[i]; i++)
-				str[i] += FOUR;
+				str[i] += de_encryptionNumber;
 			cout << "Зашифрованный текст: " << str << endl;
 			for (int u = NULL; str[u]; u++)
-				str[u] -= FOUR;
+				str[u] -= de_encryptionNumber;
 			cout << "Дешифрованный текст: " << str << "\n\n============================\n";
 		}
 		return 0;
